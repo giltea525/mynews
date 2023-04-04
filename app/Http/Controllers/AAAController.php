2df5060f6ee81708f85456566a9bd708152389d4
giltea525 <129069442+giltea525@users.noreply.github.com>
2023-04-04 14:03:02 +0000

    課題Laravel09-03_AAAController作成

---
+++ app/Http/Controllers/AAAController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AAAController extends Controller
+{
+    // 課題3：Laravel09
+    public function bbb()
+    {
+        return view('xxx');
+    }
+}
