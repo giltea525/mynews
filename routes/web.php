09c24df5edf7b2f9b2ab865b7784dc5398846747
giltea525 <129069442+giltea525@users.noreply.github.com>
2023-04-04 12:19:47 +0000

    課題Laravel09

---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+use App\Http\Controllers\Admin\NewsController;
+Route::controller(NewsController::class)->prefix('admin')->group(function()
+{
+    Route::get('news/create', 'add');
+});
+
+// 課題3：Laravel09
+use App\Http\Controllers\AAAController;
+Route::get('xxx', [AAAController::class, 'bbb']);
+
+// 課題4：Laravel09
+use App\Http\Controllers\Admin\ProfileController;
+Route::controller(ProfileController::class)->prefix('admin/profile')->group(function()
+{
+    Route::get('create', 'add');
+    Route::get('edit', 'edit');
+});
