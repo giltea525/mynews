32a2b4a7f40016e906771a4072adbc0458986796
giltea525 <129069442+giltea525@users.noreply.github.com>
2023-04-05 13:10:13 +0000

    課題Laravel10

---
+++ resources/views/admin/profile/create.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible"content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>MyNews</title>
+    </head>
+    <body>
+        <h1>プロファイル作成画面</h1>
+        <div>
+            <ul>
+                <li>氏名</li>
+                <li>Email</li>   
+                <li>自己紹介</li>
+            </ul>
+        </div>
+    </body>
+</html>
---
+++ resources/views/admin/profile/edit.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible"content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>MyNews</title>
+    </head>
+    <body>
+        <h1>Myニュース投稿画面</h1>
+        <div>
+            <h2>ニュースタイトル</h2>
+            <P>内容を書く</p>
+        </div>
+    </body>
+</html>
