085a484e523c33485fca1d2ecc549d71b400224c
giltea525 <129069442+giltea525@users.noreply.github.com>
2023-04-10 14:43:27 +0000

    課題Laravel13-03,06

--- routes/web.php
+++ routes/web.php
@@ -18,21 +18,22 @@ Route::get('/', function () {
 });
 
 use App\Http\Controllers\Admin\NewsController;
-Route::controller(NewsController::class)->prefix('admin')->group(function()
-{
-    Route::get('news/create', 'add')->middleware('auth');
+Route::controller(NewsController::class)->prefix('admin')->name('admin.')->middleware('auth')->group(function () {
+    Route::get('news/create', 'add')->name('news.add');
+    Route::post('news/create', 'create')->name('news.create');
 });
 
 // 課題3：Laravel09
 use App\Http\Controllers\AAAController;
 Route::get('xxx', [AAAController::class, 'bbb']);
 
-// 課題4：Laravel09
+// 課題4：Laravel09, 課題：Laravel12, 課題：Laravel13
 use App\Http\Controllers\Admin\ProfileController;
-Route::controller(ProfileController::class)->prefix('admin/profile')->group(function()
-{
-    Route::get('create', 'add')->middleware('auth');
-    Route::get('edit', 'edit')->middleware('auth');
+Route::controller(ProfileController::class)->prefix('admin')->name('admin.')->middleware('auth')->group(function() {
+    Route::get('profile/create', 'add')->name('profile.add');
+    Route::get('profile/edit', 'edit')->name('profile.edit');
+    Route::post('profile/create', 'create')->name('profile.create');
+    Route::post('profile/edit', 'update')->name('profile.update');
 });
 Auth::routes();
 
