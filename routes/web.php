3b73b7835591b7ccc73bcc64176ea7902d5e3dcd
giltea525 <129069442+giltea525@users.noreply.github.com>
2023-04-15 07:53:47 +0000

    課題Laravel16_editとupdateのRoutingを実装

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,13 @@ use App\Http\Controllers\Admin\NewsController;
 Route::controller(NewsController::class)->prefix('admin')->name('admin.')->middleware('auth')->group(function () {
     Route::get('news/create', 'add')->name('news.add');
     Route::post('news/create', 'create')->name('news.create');
+    // 以下を追記 Laravel15
+    Route::get('news', 'index')->name('news.index');
+    // 以下を追記 Laravel16 データの更新
+    Route::get('news/edit', 'edit')->name('news.edit');
+    Route::post('news/edit', 'update')->name('news.update');
+    // 以下を追記 Laravel16 データの削除
+    Route::get('news/delete', 'delete')->name('news.delete');
 });
 
 // 課題3：Laravel09
@@ -34,6 +41,9 @@ Route::controller(ProfileController::class)->prefix('admin')->name('admin.')->mi
     Route::get('profile/edit', 'edit')->name('profile.edit');
     Route::post('profile/create', 'create')->name('profile.create');
     Route::post('profile/edit', 'update')->name('profile.update');
+    // 課題：Laravel16
+    Route::get('profile', 'index')->name('profile.index');
+    Route::get('profile/delete', 'delete')->name('profile.delete');
 });
 Auth::routes();
 
